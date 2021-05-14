9153639ed002b3443e7f6e71b711c95778739fa2
qchea <qchea@amazon.com>
2021-05-14 16:24:32 -0500

    MAINT: add TODO
    Signed-off-by: qchea <qchea@amazon.com>

--- data-prepper-plugins/elasticsearch/src/main/java/com/amazon/dataprepper/plugins/sink/elasticsearch/ElasticsearchSink.java
+++ data-prepper-plugins/elasticsearch/src/main/java/com/amazon/dataprepper/plugins/sink/elasticsearch/ElasticsearchSink.java
@@ -193,6 +193,7 @@ public class ElasticsearchSink extends AbstractSink<Record<String>> {
           // Do nothing - likely caused by a race condition where the resource was created
           // by another host before this host's restClient made its request
         } else if (e.getMessage().contains(String.format(INDEX_ALIAS_USED_AS_INDEX_ERROR, indexAlias))) {
+          // TODO: replace IOException with custom data-prepper exception
           throw new IOException(
                   String.format("An index exists with the same name as the reserved index alias name [%s], please delete or migrate the existing index",
                           indexAlias));
