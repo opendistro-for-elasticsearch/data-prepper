5c3b66f56441abe8460c6aa9256f819b75774a3d
qchea <qchea@amazon.com>
2021-05-27 18:20:10 -0500

    FIX: spotless
    Signed-off-by: qchea <qchea@amazon.com>

--- data-prepper-benchmarks/otel-trace-raw-benchmarks/src/jmh/java/com/amazon/dataprepper/benchmarks/prepper/OtelTraceRawPrepperBenchmarks.java
+++ data-prepper-benchmarks/otel-trace-raw-benchmarks/src/jmh/java/com/amazon/dataprepper/benchmarks/prepper/OtelTraceRawPrepperBenchmarks.java
@@ -20,10 +20,8 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
 
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
